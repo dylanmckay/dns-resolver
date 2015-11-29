d868d64a154661f5775a4b3d5ef40ce17c4b4202
Lena Plaksina <lena.plaksina@gmail.com>
2015-11-30 12:02:55 +1300

    Finish name class

--- lib/name.rb
+++ lib/name.rb
@@ -5,8 +5,18 @@ class Name
   attr_reader :parts
 
   def initialize(domain)
-    # TODO
+    @parts = calculate_parts(domain)
   end
 
-end
+  def encoded_name
+    @parts.inject([]) do |bytes, part|
+      bytes + [part.length] + part.chars
+    end
+  end
 
+  private
+  
+  def calculate_parts(domain)
+    domain.split(".")
+  end
+end
