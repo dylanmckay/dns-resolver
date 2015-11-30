b1432f08bb5913f13e6f64f7c3294150efd65b04
Dylan McKay <dylan.mckay@powershop.co.nz>
2015-11-30 15:18:33 +1300

    Parse resource data but ignore it

--- lib/resource.rb
+++ lib/resource.rb
@@ -1,13 +1,14 @@
 require_relative 'util'
 
 class Resource
-  attr_reader :ip_address, :type, :class
+  attr_reader :ip_address, :type, :class, :rdata
 
-  def initialize(ip_address, type, class, ttl)
+  def initialize(ip_address, type, class, ttl, rdata = nil)
     @ip_address = ip_address
     @type = type
     @class = class
     @ttl = ttl
+    @rdata = rdata
   end
 
   def self.parse(bytes)
@@ -15,6 +16,11 @@ class Resource
     type = concatenate_bytes(bytes[4...5])
     class = concatenate_bytes(bytes[6...7])
     ttl = concatenate_bytes(bytes[8...11])
-    Resource.new(ip_address, type, class, ttl)
+
+    rdata_length = concatenate_bytes(bytes[11...13])
+
+    rdata = concatenate_bytes(bytes[13...13+rdata_length])
+
+    Resource.new(ip_address, type, class, ttl, rdata)
   end
 end
