1e3a0694655f7db69ab6d431c02b8e68c894b24c
Lena Plaksina <lena.plaksina@gmail.com>
2015-11-30 14:50:56 +1300

    Added Header.parse method

--- lib/header.rb
+++ lib/header.rb
@@ -6,7 +6,8 @@ module Packet
 
   class Header
 
-    attr_accessor :id, :query_response_flag, :opcode, :response_code, :flags
+    attr_accessor :id, :query_response_flag, :opcode, :response_code, :flags,
+                  :question_count, :answer_count, :authority_count, :additional_count
 
     def initialize
       @id = 0
@@ -14,7 +15,35 @@ module Packet
       @opcode = 0
       @response_code = 0
 
+      @question_count = 0
+      @answer_count = 0
+      @authority_count = 0
+      @additional_count = 0
+
       @flags = Flags.new
+    end
+
+    def self.parse(bits)
+      header = Header.new
+
+      header.id = concatenate_bits(bits[0...16])
+      header.query_response_flag = bits[16]
+      header.opcode = concatenate_bits(bits[17...21])
+      header.aa = bits[21]
+      header.tc = bits[22]
+      header.rd = bits[23]
+      header.ra = bits[24]
+      header.z = bits[25]
+      header.ad = bits[26]
+      header.cd = bits[27]
+      header.rcode = concatenate_bits(bits[28..31])
+
+      header.question_count = concatenate_bits(32...48)
+      header.answer_count = concatenate_bits(48...64)
+      header.authority_count = concatenate_bits(64...80)
+      header.additional_count = concatenate_bits(80...96)
+
+      header
     end
   end
 
