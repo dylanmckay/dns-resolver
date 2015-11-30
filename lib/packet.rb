921c736e911a44953b9aa0a231746e273ae58041
Dylan McKay <dylan.mckay@powershop.co.nz>
2015-11-30 15:18:50 +1300

    Add Packet.parse

--- lib/packet.rb
+++ lib/packet.rb
@@ -11,5 +11,30 @@ module Packet
       @authorities = authorities
       @additionals = additionals
     end
+
+    def self.parse(bytes)
+      header = Header.parse(bytes[0...12])
+
+      index = 12
+
+      header.question_count.times.map do
+        question_bytes = bytes[index...index+4]
+        index += 4
+
+        Query.parse(question_bytes)
+      end
+
+      header.answer_count.times.map do
+        rdata_bytes = bytes[index..-1]
+        resource = Resource.parse(rdata_bytes)
+
+        index += 14 + resource.rdata.length
+
+        resource
+      end
+
+      # TODO: read other RRs
+
+    end
   end
 end
